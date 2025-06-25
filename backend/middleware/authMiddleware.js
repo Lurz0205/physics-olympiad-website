a4d96b33d68bb273c1b6e09acce93f1ba006465e
Lurz0205 <91960598+Lurz0205@users.noreply.github.com>
2025-06-25 22:28:39 +0700

    Create authMiddleware.js

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const protect = async (req, res, next) => {
+  let token;
+
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    try {
+      // Lấy token từ header
+      token = req.headers.authorization.split(' ')[1];
+
+      // Giải mã token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      // Tìm user theo ID trong token và gán vào req.user
+      req.user = await User.findById(decoded.id).select('-password'); // Bỏ qua trường password
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(401).json({ message: 'Không được ủy quyền, token lỗi' });
+    }
+  }
+
+  if (!token) {
+    res.status(401).json({ message: 'Không được ủy quyền, không có token' });
+  }
+};
+
+module.exports = { protect };
