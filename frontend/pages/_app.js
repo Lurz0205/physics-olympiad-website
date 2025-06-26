a9af59d5785376b339c75b291c19b70c1b24f5c3
Lurz0205 <91960598+Lurz0205@users.noreply.github.com>
2025-06-26 09:47:10 +0700

    Update _app.js

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,11 +1,21 @@
-import '../styles/globals.css'; // Đảm bảo dòng này đã được import
-import Layout from '../components/Layout';
+// physics-olympiad-website/frontend/pages/_app.js
+import '../styles/globals.css';
+import { AuthProvider } from '../context/AuthContext';
+import Layout from '../components/Layout'; // Giả sử bạn có component Layout
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <AuthProvider>
+      {/* Thay đổi màu nền của div bọc toàn bộ ứng dụng */}
+      {/* Ví dụ: bg-white hoặc bg-gray-50 */}
+      <div className="bg-white min-h-screen"> {/* Đã sửa bg-red-200 thành bg-white */}
+        <Layout> {/* Nếu bạn có Layout component */}
+          <Component {...pageProps} />
+        </Layout>
+        {/* Hoặc nếu không có Layout: */}
+        {/* <Component {...pageProps} /> */}
+      </div>
+    </AuthProvider>
   );
 }
 
