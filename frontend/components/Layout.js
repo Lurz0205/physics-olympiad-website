b2103c1d63f47b4b7bd2fa598d8cf51475334b16
Lurz0205 <91960598+Lurz0205@users.noreply.github.com>
2025-06-26 10:42:09 +0700

    Update Layout.js

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -1,18 +1,14 @@
 // physics-olympiad-website/frontend/components/Layout.js
 import React from 'react';
-import Navbar from './Navbar'; // Import Navbar component
+import Navbar from './Navbar';
 
 const Layout = ({ children }) => {
   return (
-    <div className="min-h-screen flex flex-col bg-gray-50"> {/* Nền xám nhạt tinh tế cho toàn bộ nội dung */}
-      <Navbar /> {/* Thanh điều hướng */}
-      <main className="flex-grow container mx-auto p-4 sm:p-6 lg:p-8"> {/* Container cho nội dung chính */}
+    <div className="min-h-screen flex flex-col bg-gray-50">
+      <Navbar />
+      <main className="flex-grow container mx-auto p-4 sm:p-6 lg:p-8"> {/* Padding responsive */}
         {children}
       </main>
-      {/* Bạn có thể thêm Footer ở đây nếu có */}
-      {/* <footer className="bg-gray-800 text-white p-4 text-center mt-auto">
-        Bản quyền © 2024 Olympic Vật lý HSG
-      </footer> */}
     </div>
   );
 };
