c7854b074aefdc7b45633bafb4966c35e207298d
Lurz0205 <91960598+Lurz0205@users.noreply.github.com>
2025-06-26 08:26:46 +0700

    Update PracticeTopic.js

--- backend/models/PracticeTopic.js
+++ backend/models/PracticeTopic.js
@@ -1,29 +1,30 @@
-// physics-olympiad-website/backend/models/PracticeTopic.js
-const mongoose = require('mongoose');
+            // physics-olympiad-website/backend/models/PracticeTopic.js
+            const mongoose = require('mongoose');
 
-const practiceTopicSchema = mongoose.Schema(
-  {
-    title: {
-      type: String,
-      required: true,
-      unique: true, // Ensure topic titles are unique
-    },
-    slug: {
-      type: String,
-      required: true,
-      unique: true, // Ensure slugs are unique for URL purposes
-    },
-    description: {
-      type: String,
-      required: true,
-    },
-    // You can add more fields here if needed, e.g., 'difficulty', 'image'
-  },
-  {
-    timestamps: true, // Adds createdAt and updatedAt timestamps
-  }
-);
+            const practiceTopicSchema = mongoose.Schema(
+              {
+                title: {
+                  type: String,
+                  required: true,
+                  unique: true, // Ensure topic titles are unique
+                },
+                slug: {
+                  type: String,
+                  required: true,
+                  unique: true, // Ensure slugs are unique for URL purposes
+                },
+                description: {
+                  type: String,
+                  required: true,
+                },
+                // You can add more fields here if needed, e.g., 'difficulty', 'image'
+              },
+              {
+                timestamps: true, // Adds createdAt and updatedAt timestamps
+              }
+            );
 
-const PracticeTopic = mongoose.model('PracticeTopic', practiceTopicSchema);
+            const PracticeTopic = mongoose.model('PracticeTopic', practiceTopicSchema);
 
-module.exports = PracticeTopic;
+            module.exports = PracticeTopic;
+            
