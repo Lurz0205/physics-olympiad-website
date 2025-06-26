78ccdbbfa33304ab28bcce7fd2f9ae4f9e38b43c
Lurz0205 <91960598+Lurz0205@users.noreply.github.com>
2025-06-26 20:04:57 +0700

    Update _document.js

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -8,14 +8,13 @@ export default function Document() {
         {/* Favicon - Đảm bảo bạn có file favicon.ico trong thư mục public */}
         <link rel="icon" href="/favicon.ico" />
 
-        {/* Google Fonts - Inter (hoặc font khác bạn muốn dùng chung) */}
-        {/* Đảm bảo tải subset tiếng Việt để hiển thị tốt hơn */}
+        {/* Google Fonts - Inter */}
+        {/* THAY ĐỔI QUAN TRỌNG: font-display=block để loại bỏ CLS do font */}
         <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap&subset=vietnamese"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=block&subset=vietnamese" // Đã thay đổi 'swap' thành 'block'
           rel="stylesheet"
         />
-        {/* THAY ĐỔI MỚI: Preload các trọng số (weights) của font Inter mà bạn đang sử dụng */}
-        {/* Preload giúp trình duyệt tải font sớm hơn, giảm CLS */}
+        {/* Preload các trọng số (weights) của font Inter mà bạn đang sử dụng */}
         <link rel="preload" href="https://fonts.gstatic.com/s/inter/v13/UcCOyFect0rcMhC-CcI.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
         <link rel="preload" href="https://fonts.gstatic.com/s/inter/v13/UcCOyFect0rcMhC-Boc.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
         <link rel="preload" href="https://fonts.gstatic.com/s/inter/v13/UcCOyFect0rcMhC-BLc.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
@@ -23,7 +22,7 @@ export default function Document() {
         <link rel="preload" href="https://fonts.gstatic.com/s/inter/v13/UcCOyFect0rcMhC-BGc.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
         
 
-        {/* KaTeX CSS - RẤT QUAN TRỌNG: StyleSheet cho KaTeX */}
+        {/* KaTeX CSS - StyleSheet cho KaTeX */}
         <link
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.css"
@@ -32,14 +31,14 @@ export default function Document() {
 
         {/* KaTeX JS Core - Thư viện lõi KaTeX */}
         <script
-          defer // Defer để script không chặn render HTML
+          defer
           src="https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.js"
           crossOrigin="anonymous"
         ></script>
         
         {/* KaTeX Auto-render Extension - Mở rộng để tự động render LaTeX */}
         <script
-          defer // Defer để script không chặn render HTML
+          defer
           src="https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/contrib/auto-render.min.js"
           crossOrigin="anonymous"
         ></script>
