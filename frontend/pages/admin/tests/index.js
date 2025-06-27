94fb89d5f27e01d530eee628cc6999d5b1474705
Lurz0205 <91960598+Lurz0205@users.noreply.github.com>
2025-06-27 11:03:42 +0700

    Update index.js

--- frontend/pages/admin/tests/index.js
+++ frontend/pages/admin/tests/index.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect, useCallback } from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { useAuth } from '../../../../context/AuthContext';
+import { useAuth } from '../../../context/AuthContext';
 
 const AdminTestsPage = () => {
   const router = useRouter();
